9f1db764f8a2ddcef3036ed8663d9fcb045d7122
Seeam Islam <seeamislam25@gmail.com>
2019-10-17 11:36:47 -0400

    Update: URL & Date fetch

--- Tweet Sentiment.py
+++ Tweet Sentiment.py
@@ -7,9 +7,22 @@ twitter_api = twitter.Api(consumer_key=config.ACCESS_TOKEN,
                           access_token_key=config.CONSUMER_KEY,
                           access_token_secret=config.CONSUMER_SECRET)
 
-# test authentication
-print(twitter_api.VerifyCredentials())
+
+def collectTweets(keyword):  # function to collect tweets with a specified keyword
+    try:  # testing this block of code for errors
+        tweets_fetched = twitter_api.GetSearch(keyword, count=15)  # collect first 25 tweets with the keyword
+
+        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + keyword)
+
+        return [{"text": status.text, "created_at": status.created_at, "URL": status.urls, "label": None} for status in tweets_fetched]
+        # print the text and label (placeholder for polarity) for each tweet recovered
+
+    except:  # if an error persists, run this
+        print("Unfortunately, there was an error. Try again!")
+        return None
 
 
+search_term = input("Enter a search keyword:")
+testDataSet = collectTweets(search_term)
 
-
+print(testDataSet[0:5])
