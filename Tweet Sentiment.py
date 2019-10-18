6affd3cfb5429bdeee6446b1a46d0f3439ce4c5b
Seeam Islam <seeamislam25@gmail.com>
2019-10-18 16:08:45 -0400

    Update: Relocate Flask connection

--- Tweet Sentiment.py
+++ Tweet Sentiment.py
@@ -1,18 +1,5 @@
 import twitter  # import the Twitter library
 import config  # import contents of .config
-from flask import Flask
-
-app = Flask(__name__)  # Initialize a new Flask object
-
-
-@app.route('/')
-def home():
-    return "<h1> Hello World </h1>"
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
-
 
 file = open("tweets.txt", "w+")
 
@@ -24,24 +11,18 @@ twitter_api = twitter.Api(consumer_key=config.ACCESS_TOKEN,
 
 
 def collectTweets(keyword):  # function to collect tweets with a specified keyword
-    try:  # testing this block of code for errors
-        tweets_fetched = twitter_api.GetSearch(keyword, count=10)  # collect first 25 tweets with the keyword
+    tweets_fetched = twitter_api.GetSearch(keyword, count=10)  # collect first 25 tweets with the keyword
 
-        # print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + keyword)
+    # print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + keyword)
 
-        tweet_info = [
-            {"Tweet": status.text,
-             "Create Date": status.created_at,
-             "URL": status.urls,
-             "label": None}
-            for status in tweets_fetched]
+    tweet_info = [{
+        "Tweet": status.text,
+        "Create Date": status.created_at,
+        "URL": status.urls,
+        "label": None}
+        for status in tweets_fetched]
 
-        return tweet_info
-        # print the text and label (placeholder for polarity) for each tweet recovered
-
-    except:  # if an error persists, run this
-        print("Unfortunately, there was an error. Try again!")
-        return None
+    return tweet_info
 
 
 search_term = input("Enter a search keyword:")
@@ -54,6 +35,4 @@ with open('tweets.txt', 'w+') as f:
         print("\n" + " \n")
 
 file.close()
-
-
 
